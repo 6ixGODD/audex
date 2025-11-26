25c21ecaf0f1a7e536abf2bcd4220d015a70533e
6GOD <55304045+6ixGODD@users.noreply.github.com>
2025-11-26 22:20:35 +0800

    feat(net): add functions to retrieve local IP address and free port

---
+++ audex/helper/net.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import socket
+
+
+def getaddr() -> str:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+    finally:
+        s.close()
+
+
+def getfreeport() -> int:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("", 0))
+        return s.getsockname()[1]
