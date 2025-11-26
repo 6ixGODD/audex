14f4a8b03e4e2327edab70376b0fab39c14eb3b5
6GOD <55304045+6ixGODD@users.noreply.github.com>
2025-11-26 22:21:15 +0800

    feat(cli): register subparsers for run and serve commands in Audex application

--- audex/cli/apis/__init__.py
+++ audex/cli/apis/__init__.py
@@ -1,1 +1,14 @@
 from __future__ import annotations
+
+import argparse
+
+
+def register_apis(subparsers: argparse._SubParsersAction[argparse.ArgumentParser]) -> None:
+    from audex.cli.apis import init
+    from audex.cli.apis import run
+    from audex.cli.apis import serve
+
+    run.Args.register_subparser(subparsers, name="run", help_text="Run Audex Application")
+    serve.Args.register_subparser(subparsers, name="serve", help_text="Serve Audex export service")
+
+    init.register(subparsers)  # Subparser registered inside function
